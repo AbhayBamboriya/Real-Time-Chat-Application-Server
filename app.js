2bfc006c2eac8b6df76ee7c1af2b21f19d12cf77
AbhayBamboriya <bamboriya09@gmail.com>
2024-05-23 02:07:07 +0530

    Register and login is successfull

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express =require('express')
 const app=express()
 const port=process.env.PORT||8000
+const bcryptjs=require('bcryptjs')
+const jwt=require('jsonwebtoken')
 // require('./db/connection.js')
 const connectionToDB=require('./db/connection')
 const Users=require('./models/User')
@@ -9,6 +11,76 @@ app.use(express.urlencoded({extended:false}))
 
 app.get('/',(req,res)=>{
     res.send('welcome')
+})
+app.post('/api/register',async (req,res,next)=>{
+    try{
+        const {fullName,email,password}=req.body
+        if(!fullName || !email || !password){
+            res.status(400).send('Please fill all entries')
+        }
+        else{
+            const isExist=await Users.findOne({email})
+            if(isExist){
+                res.status(400).send('User already Exist')
+            }
+            else{
+                const newUser=new Users({
+                    fullName,
+                    email
+                })
+                bcryptjs.hash(password,10,(err,hashedPasword)=>{
+                    newUser.set('password',hashedPasword)
+                    newUser.save()
+                    next();
+                })
+                return res.status(200).send('User is registered Successfully')
+            }
+        }
+    }
+    catch(e){
+        console.log(e.message);
+    }
+})
+
+app.post('/api/login', async (req,res,next)=>{
+    try{
+        const {email,password}=req.body
+        if(!email || !password){
+            res.status(400).send('Please fill all entries')
+        }
+        else{
+            const user=await Users.findOne({email})
+            if(!user){
+                res.status(400).send('Please enter valid email Id')
+            }
+            else{
+               const validateUser=await bcryptjs.compare(password,user.password)
+               if(!validateUser){
+                    res.status(400).send('Enter Valid password')
+               }
+               else{
+                    const payload={
+                        userId:user._id,
+                        email:user.email
+                    }
+                    const JWT_SECRET_KEY=process.env.JWT_SECRET_KEY||'534'
+
+                    jwt.sign(payload,JWT_SECRET_KEY,{expiresIn:84600},async(err,token)=>{
+                        await Users.updateOne({_id:user._id},{
+                            $set:{token}
+                        })
+                        user.save()
+                        next()
+                    })
+                    res.status(200).json({user:{email:user.email,fullName:user.fullName},token:user.token})
+               }
+
+            }
+        }
+    }
+    catch(e){
+        console.log(e.message);
+    }
 })
 app.listen(port,async()=>{
     await connectionToDB()
